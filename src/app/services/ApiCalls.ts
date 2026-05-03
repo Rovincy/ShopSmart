4bc55d3304c3d6ae6a6a9ec8ed6f2c3bb31e387f
Rovincy <destaeldosso@gmail.com>
2026-05-03 18:50:44 +0000

    api update

--- src/app/services/ApiCalls.ts
+++ src/app/services/ApiCalls.ts
@@ -5,7 +5,8 @@ import axios from 'axios'
 // export const Report_URL = "http://localhost/HRMS-Reports/"
 // export const Report_URL = "https://15.236.239.129/2i-reports/"
 export const Report_URL = "https://2i-institut.com/2i-reports/"
-export const Api_Endpoint = 'https://localhost:7294/api'
+export const Api_Endpoint = 'https://api.rovincy.com/api'
+// export const Api_Endpoint = 'https://localhost:7294/api'
 // export  const Api_Endpoint ="https://15.236.239.129/2i-api/api";
 // export  const Api_Endpoint ="https://2i-institut.com/2i-api/api";
 // 
