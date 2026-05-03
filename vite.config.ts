5d17c00aa463a435120b8ba580236de7454da133
Rovincy <destaeldosso@gmail.com>
2026-05-03 18:46:11 +0000

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ShopSmart/",
+  base: '/',                    // ← Add or change this to '/'
+  
   preview: {
-    host: true,           // Listen on all addresses
+    host: true,
     port: 4173,
-    allowedHosts: ['smartshop.rovincy.com', '.rovincy.com', 'localhost', '127.0.0.1']
-  },
-  build: {
-    chunkSizeWarningLimit: 3000,
-  },
+    allowedHosts: ['smartshop.rovincy.com', '.rovincy.com']
+  }
 })
